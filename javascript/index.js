57354223044ea74de9577bb681ce2827344179d9
itzmeowww <winthanasan@gmail.com>
2020-03-28 01:28:58 +0700

    minor update

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $(".name-container-text").addClass("fade-in");
+
+    setTimeout(function() {
+        $(".name-container-text").removeClass("fade-in");
+    }, 2000);
+
+});
+
+$(".name-container-text").hover(function() {
+    var obj = $(this);
+    obj.addClass("name-container-text-animation");
+    setTimeout(function() {
+        obj.removeClass("name-container-text-animation");
+        console.log("REMOVED");
+    }, 1000);
 });
