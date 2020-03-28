c1fc3964d71963c3f50ff21524bb14d0dc949162
itzmeowww <winthanasan@gmail.com>
2020-03-29 03:52:53 +0700

    Create gallery page

---
+++ pages/javascript/gallery.js
@@ -0,0 +1,72 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAh6_jWRi5sM87CLQG62yFN6HgdG35-iSQ",
+    authDomain: "itzmeo-github-io.firebaseapp.com",
+    databaseURL: "https://itzmeo-github-io.firebaseio.com",
+    projectId: "itzmeo-github-io",
+    storageBucket: "itzmeo-github-io.appspot.com",
+    messagingSenderId: "670423305836",
+    appId: "1:670423305836:web:1ad3ee041a3660765e9249",
+    measurementId: "G-8MVVWVBVNR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var storage = firebase.storage();
+var storageRef = firebase.storage().ref();
+var imagesRef = storageRef.child('gallery');
+
+
+
+$(".my-img").hide();
+
+
+function updateDesc(elem, val) {
+    elem.children(".text").children("h3").text('Text ' + val);
+}
+
+imagesRef.listAll().then(function(res) {
+    res.items.forEach(function(item) {
+        console.log(item.name);
+        var fileName = item.name;
+        var img = imagesRef.child(fileName);
+        img.getDownloadURL().then(function(url) {
+            var elem = $(".my-img:last");
+
+            $(".gallery").append(elem.clone());
+            var nosuf = fileName.split('.').slice(0, -1).join('.');
+
+
+
+
+            var galleryRef = firebase.database().ref('/gallery/' + nosuf);
+            galleryRef.on('value', function(snapshot) {
+                updateDesc(elem, snapshot.val().description);
+            });
+
+            elem.children(".img-container").children(".img").attr("src", url);
+
+
+            elem.show();
+            console.log('Add ' + fileName + ' ' + url);
+
+        }).catch(function(error) {
+            console.log('Error!!');
+            console.log(error)
+        });
+    });
+}).catch(function(error) {
+    console.log('Error');
+    console.log(error);
+});
+
+
+
+
+// $(".my-img").hover(function() {
+//     console.log("hover");
+//     jQuery(this).children(".text").show();
+// }, function() {
+//     console.log("hover");
+//     jQuery(this).children(".text").hide();
+// });
