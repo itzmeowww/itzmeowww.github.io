b2885ea1c7ed7eb805a5dd726f598b9e0aa23dc3
itzmeowww <winthanasan@gmail.com>
2020-04-09 17:02:49 +0700

    TTS

--- pages/javascript/oreo.js
+++ pages/javascript/oreo.js
@@ -124,7 +124,8 @@ $(document).ready(function() {
         win.document.write('<iframe src="' + base64URL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
     }
 
-
+    var player = new talkify.TtsPlayer(); //or new talkify.Html5Player()
+    player.playText('Hello world');
 
     $("#btnSave").click(function() {
         var btn = $(this);
