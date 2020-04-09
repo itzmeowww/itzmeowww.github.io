63d726e1c1956c6023b7f16b6419389d74786435
itzmeowww <winthanasan@gmail.com>
2020-04-09 17:16:27 +0700

    Fix bug for mobile

--- pages/javascript/oreo.js
+++ pages/javascript/oreo.js
@@ -124,8 +124,6 @@ $(document).ready(function() {
         win.document.write('<iframe src="' + base64URL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
     }
 
-    var player = new talkify.TtsPlayer(); //or new talkify.Html5Player()
-    player.playText('Hello world');
 
     $("#btnSave").click(function() {
         var btn = $(this);
@@ -133,9 +131,6 @@ $(document).ready(function() {
         btn.text('Saving');
         html2canvas($("#my-oreo")[0]).then(function(canvas) {
             btn.text('Save PNG');
-            canvas.backgroundColor = null;
-
-
             var url = canvas.toDataURL("image/png".replace("image/png", "image/octet-stream"));
             debugBase64(url);
             /**
