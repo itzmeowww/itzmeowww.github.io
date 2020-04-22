61f85e748f7b37ba43f67567ad900172cc8d8c6b
itzmeowww <winthanasan@gmail.com>
2020-04-22 16:19:31 +0700

    ball.html

--- pages/javascript/ball.js
+++ pages/javascript/ball.js
@@ -1,3 +1,12 @@
+var isMobile = false;
+if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    )
+) {
+    isMobile = true;
+}
+
 var two = new Two({
     type: Two.Types["canvas"],
     fullscreen: true,
@@ -9,8 +18,18 @@ circle.fill = "#FF8000";
 circle.stroke = "orangered"; // Accepts all valid css color
 circle.linewidth = 5;
 circle.translation.set(two.width / 2, two.height / 2);
+
+var styles = {
+    family: "proxima-nova, sans-serif",
+    size: isMobile ? 20 : 50,
+    leading: 50,
+    weight: 900,
+};
+
+var text = two.makeText("Hi", two.width / 2, two.height / 2, styles);
 two.bind("resize", function () {
     circle.translation.set(two.width / 2, two.height / 2);
+    text.translation.set(two.width / 2, two.height / 2);
 }).bind("update", function () {
     if (
         circle.translation.x + force.x >= 0 &&
@@ -34,12 +53,14 @@ function handleOrientation(event) {
     var gamma = event.gamma;
     var str = absolute + " " + alpha + " " + beta + " " + gamma;
     if (beta == 0) force.y = 0;
-    else if (beta > 0) force.y = 1;
-    else force.y = -1;
+    else if (beta > 0) force.y = beta / 10;
+    else force.y = beta / 10;
 
     if (gamma == 0) force.x = 0;
-    else if (gamma > 0) force.x = 1;
-    else force.x = -1;
+    else if (gamma > 0) force.x = gamma / 10;
+    else force.x = gamma / 10;
     // $("#show").text(str);
+    if (beta != null && gamma != null)
+        text.value = Math.round(beta) + " " + Math.round(gamma);
     console.log(force);
 }
